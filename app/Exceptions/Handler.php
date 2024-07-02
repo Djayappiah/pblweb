2e9509ae2839ab3097fb5079df7015e4cb3d85a5
Ransford Boakye <Ransford.Boakye@prudentialbank.local>
2024-07-02 18:22:23 +0000

    Updates

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -27,4 +27,20 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    function render($request, Throwable $exception)
+    {
+            if ($this->isHttpException($exception)) {
+                if ($exception->getStatusCode() == 401) {
+                    return response()->view('errors.401', [], 401);
+                }
+                if ($exception->getStatusCode() == 500) {
+                    return response()->view('errors.500', [], 500);
+                }
+                if ($exception->getStatusCode() == 503) {
+                    return response()->view('errors.503', [], 503);
+                }
+            }
+            return parent::render($request, $exception);
+        }
 }
